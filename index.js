a34c78091bfad045de85d21e8f7db1bfefc19ee7
KamatchiKarthi <karthivdj@gmail.com>
2025-09-12 00:16:51 +0530

    port change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.DB_PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
